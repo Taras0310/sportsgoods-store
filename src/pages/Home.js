59a66b2b373866f7b09f3c05535dc534393ce351
Taras Yakushevych <taras03101999@ukr.net>
2022-05-17 17:33:43 +0300

    test commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Api from "../api";
 import CategoryList from "../component/CategoryList";
 import { useStore } from "../contexts/AppContext";
